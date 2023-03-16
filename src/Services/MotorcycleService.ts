19cb27b8d1f9bd5d6b0554833b9337cc8454af90
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-16 20:35:45 -0300

    feat(req05): add Motorcycle service with create

---
+++ src/Services/MotorcycleService.ts
@@ -0,0 +1,17 @@
+import AbstractODM from '../Models/AbstractODM';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import IMotorcycleService from './interfaces/IMotorcycleService';
+import Motorcycle from '../Domains/Motorcycle';
+
+export default class MotorcycleService implements IMotorcycleService {
+  constructor(private _motorcycleModel: AbstractODM<IMotorcycle>) {}
+
+  public async create(motorcycle: IMotorcycle): Promise<Motorcycle> {
+    const { model, year, color, status, buyValue, category, engineCapacity } = motorcycle;
+    const newMotorcycle = await this._motorcycleModel.create(
+      { model, year, color, status: status || false, buyValue, category, engineCapacity },
+    );
+
+    return new Motorcycle(newMotorcycle);
+  }
+}
