0d14933665124b0023944de44a9673d72036d5d6
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-16 13:21:35 -0300

    feat(req01): add Car router

---
+++ src/Routers/CarRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import CarController from '../Controllers/CarController';
+import CarModel from '../Models/CarModel';
+import CarService from '../Services/CarService';
+
+const carRouter = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+carRouter.post('/', carController.create);
+
+export default carRouter;
