e8890f3934b66fd00a55bbe500ab033b80694784
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-16 18:50:51 -0300

    feat: add validateId in AbstractODM

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,4 +1,6 @@
-import { Model, model, models, Schema } from 'mongoose';
+import { isValidObjectId, Model, model, models, Schema } from 'mongoose';
+
+import HttpException from '../utils/HttpException';
 
 export default abstract class AbstractODM<T> {
   protected schema: Schema;
@@ -9,6 +11,12 @@ export default abstract class AbstractODM<T> {
     this.schema = schema;
     this.modelName = modelName;
     this.model = models[modelName] || model(modelName, schema);
+  }
+
+  private _validateId(id: string): void {
+    if (!isValidObjectId(id)) {
+      throw new HttpException(422, 'Invalid mongo id');
+    }
   }
 
   public async create(obj: T): Promise<T> {
