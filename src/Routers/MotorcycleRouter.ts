69ca06a491ec60b2dcf2dfa6eaffc50b69ca5ff8
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-16 20:37:49 -0300

    feat(req05): add Motorcycle router with create

---
+++ src/Routers/MotorcycleRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import MotorcycleController from '../Controllers/MotorcycleController';
+import MotorcycleModel from '../Models/MotorcycleModel';
+import MotorcycleService from '../Services/MotorcycleService';
+
+const motorcycleRouter = Router();
+
+const motorcycleModel = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycleModel);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+motorcycleRouter.post('/', motorcycleController.create);
+
+export default motorcycleRouter;
--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import carRouter from './CarRouter';
+import motorcycleRouter from './MotorcycleRouter';
 
 const router = Router();
 
 router.use('/cars', carRouter);
+router.use('/motorcycles', motorcycleRouter);
 
 export default router;
