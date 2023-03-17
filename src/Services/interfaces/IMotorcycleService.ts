aa3573c05def0a5d0fe282f62a5e22ea974247c1
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-16 21:25:45 -0300

    feat(req07): add getById in Motorcycle service interface

--- src/Services/interfaces/IMotorcycleService.ts
+++ src/Services/interfaces/IMotorcycleService.ts
@@ -4,4 +4,5 @@ import Motorcycle from '../../Domains/Motorcycle';
 export default interface IMotorcycleService {
   create(motorcycle: IMotorcycle): Promise<Motorcycle>;
   getAll(): Promise<Motorcycle[]>;
+  getById(id: string): Promise<Motorcycle | null>;
 }
